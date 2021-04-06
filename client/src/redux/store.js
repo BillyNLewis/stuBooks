ba982bed9c4299b51f6d6c7ffc5d678f7e635bf4
Billy Lewis <billy@Billys-MacBook-Pro.local>
2021-04-05 23:43:01 -0400

    update redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,6 +2,6 @@ import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
-
+//create redux store for application
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 export default store;
